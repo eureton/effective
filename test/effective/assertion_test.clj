93d525c899656a167832f2de7ec709b014f7716d
Agis Theodoropoulos <agis@eureton.com>
2023-09-25 10:41:31 +0300

    Unit test :to for assertion/make

--- test/effective/assertion_test.clj
+++ test/effective/assertion_test.clj
@@ -9,3 +9,11 @@
     (is (= (symbol #'=) operator))
     (is (= 4 expected))
     (is (= 'before-3 actual))))
+
+(deftest to
+  (let [assertions (make {:to -2} 2)
+        [[_ [operator expected actual] _]] assertions]
+    (is (= 1 (count assertions)))
+    (is (= (symbol #'=) operator))
+    (is (= -2 expected))
+    (is (= 'after-2 actual))))
