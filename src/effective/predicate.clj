ea0479ebf35b7a610a7f60246e0f75b888f90514
Agis Theodoropoulos <agis@eureton.com>
2023-09-25 08:54:36 +0300

    Scratch: generate before and after checkpoints

--- src/effective/predicate.clj
+++ src/effective/predicate.clj
@@ -14,13 +14,23 @@
   "Processes the multi-monitor configuration (see `effective.core/effect`)
    and the effect form. Produces a vector of predicates."
   [form config]
-  `(let [make-watcher# #(fn []
-                          (get-in ~config [% :changes]))
-         unimonitor# (->> ~config
-                          (map-indexed #(-> %2
-                                            (assoc :watch-fn (make-watcher# %1))
-                                            (dissoc :changes)
-                                            (monitor/make)))
-                          (reduce monitor/compose))
-         seed# (fn [] ~form [])]
-     (unimonitor# seed#)))
+  (let [before-vars (interleave
+                      (map #(symbol (str "before-" %)) (range))
+                      (map :changes config))
+        after-vars (interleave
+                      (map #(symbol (str "after-" %)) (range))
+                      (map :changes config))]
+    `(let [make-watcher# #(fn []
+                            (get-in ~config [% :changes]))
+           unimonitor# (->> ~config
+                            (map-indexed #(-> %2
+                                              (assoc :watch-fn (make-watcher# %1))
+                                              (dissoc :changes)
+                                              (monitor/make)))
+                            (reduce monitor/compose))
+           seed# (fn [] ~form [])]
+       (let [~@before-vars
+            ;_# ~form
+             ~@after-vars]
+         (true? false))
+       (unimonitor# seed#))))
