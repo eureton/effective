b5e69bd6d5f70ffb6e3651d11dc05ae1d63274da
Agis Theodoropoulos <agis@eureton.com>
2023-09-25 10:00:04 +0300

    Generate clojure.test assertions

---
+++ src/effective/assertion.clj
@@ -0,0 +1,15 @@
+(ns effective.assertion
+  (:require [clojure.test :refer [is]]))
+
+(defn- prefixed-symbol [prefix index]
+  (symbol (str prefix "-" index)))
+
+(defn make
+  [config index]
+  (let [{:keys [from to by]} config
+        before (prefixed-symbol "before" index)
+        after (prefixed-symbol "after" index)]
+    (cond-> []
+      from (conj `(is (= ~from ~before            ":from check failed")))
+      to   (conj `(is (= ~to   ~after               ":to check failed")))
+      by   (conj `(is (= ~by   (- ~after ~before)   ":by check failed"))))))
