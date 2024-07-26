b9bd864434766ff6b30629d92303ccbeceba61eb
Agis Theodoropoulos <agis@eureton.com>
2024-07-26 23:31:43 +0300

    Update terminology

--- src/effective/checkpoint.clj
+++ src/effective/checkpoint.clj
@@ -1,13 +1,13 @@
 (ns effective.checkpoint
-  "Symbolic names for referencing the state of the monitored expressions.")
+  "Symbolic names for referencing the state of the observables.")
 
 (defn- prefixed-symbol [prefix index]
   (symbol (str prefix "-" index)))
 
 (def before
-  "Symbol generator for the before-state of the monitored expression."
+  "Symbol generator for the before-state of the observable."
   (partial prefixed-symbol "before"))
 
 (def after
-  "Symbol generator for the after-state of the monitored expression."
+  "Symbol generator for the after-state of the observable."
   (partial prefixed-symbol "after"))
