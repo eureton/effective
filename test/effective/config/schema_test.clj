dd41329bcf409b50eb72ae177fabe9e04d71ffcb
Agis Theodoropoulos <agis@eureton.com>
2024-07-30 12:20:31 +0300

    Negative tests for function arguments

--- test/effective/config/schema_test.clj
+++ test/effective/config/schema_test.clj
@@ -3,8 +3,17 @@
             [malli.core :as m]
             [effective.config.schema :refer [hook]]))
 
-(deftest hook-function
+(deftest hook-quoted-function
   (is (m/validate hook 'odd?)))
 
 (deftest hook-symbol-headed-list
   (is (m/validate hook '(constantly true))))
+
+(deftest hook-function
+  (is (not (m/validate hook odd?))))
+
+(deftest hook-number
+  (is (not (m/validate hook 42))))
+
+(deftest hook-string
+  (is (not (m/validate hook "abc"))))
