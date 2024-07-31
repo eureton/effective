0fed05f2d81a4b76d23c9b6c9660dca20ded4d76
Agis Theodoropoulos <agis@eureton.com>
2024-07-31 11:40:47 +0300

    Entry functions for checking config validity

--- src/effective/config.clj
+++ src/effective/config.clj
@@ -1,12 +1,26 @@
 (ns effective.config
-  (:require [clojure.set :as cljset]))
-
-(def ^:const OPERATIONS #{:to-change :to-not-change :to-conjoin :to-pop})
+  (:require [clojure.set :as cljset]
+            [malli.core :as m]
+            [malli.error :as me]
+            [malli.transform :as mt]
+            [effective.config.constants :as const]
+            [effective.config.schema :as schema]))
 
 (defn operation [entry]
-  (->> entry (keys) (set) (cljset/intersection OPERATIONS) (first)))
+  (->> entry (keys) (set) (cljset/intersection const/OPERATIONS) (first)))
 
 (defn observables
   "Sequence of declared observables."
   [config]
-  (map (apply some-fn OPERATIONS) config))
+  (map (apply some-fn const/OPERATIONS) config))
+
+(def decode
+  (m/decoder schema/root mt/default-value-transformer))
+
+(def valid?
+  (comp (m/validator schema/root) decode))
+
+(def errors
+  (comp me/humanize
+        (m/explainer schema/root)
+        decode))
