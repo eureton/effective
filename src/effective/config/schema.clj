dc295707ad814add1b4ab7243b7f0ac0686869d5
Agis Theodoropoulos <agis@eureton.com>
2024-08-05 11:57:16 +0300

    Remove unused schema
    There is little sense in attempting to validate this input. Firstly, it
    is a nigh impossible task because the value could even be a Java object
    which implements IFn. Secondly, the user is explicitly marking the input
    as a function and, therefore, assumes responsibility.

--- src/effective/config/schema.clj
+++ src/effective/config/schema.clj
@@ -6,12 +6,6 @@
 
 (def ^:private ^:const OPT {:optional true})
 
-(def hook
-  "True if input represents a function, false otherwise."
-  [:or
-   [:and symbol? [:fn (comp fn? var-get resolve)]]
-   [:and list? [:fn (comp symbol? first)]]])
-
 (def observable
   "Valid if input represents an expression."
   [:or symbol? list? seq?])
--- test/effective/config/schema_test.clj
+++ test/effective/config/schema_test.clj
@@ -4,24 +4,6 @@
             [malli.transform :as mt]
             [effective.config.schema :as schema]))
 
-(deftest hook-quoted-function
-  (is (m/validate schema/hook 'odd?)))
-
-(deftest hook-symbol-headed-list
-  (is (m/validate schema/hook '(constantly true))))
-
-(deftest hook-invalid
-  (are [v] (not (m/validate schema/hook v))
-       nil
-       :x
-       "abc"
-       odd?
-       42
-       false
-       {:x 42}
-       [:x :y]
-       #{:x :y}))
-
 (deftest observable-deref
   (is (m/validate schema/observable '@x)))
 
