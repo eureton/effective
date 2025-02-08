f8546658f7b8109417b6e12c59ca2008ee539dcb
Agis Theodoropoulos <agis@eureton.com>
2025-02-08 13:02:54 +0200

    Exclude problematic dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "MIT"
             :url "https://github.com/eureton/effective/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.flatland/useful "0.11.6"]
+                 [org.flatland/useful "0.11.6"
+                  :exclusions [org.clojure/tools.macro
+                               org.clojure/tools.reader]]
                  [metosin/malli "0.16.2"]]
   :repl-options {:init-ns effective.core})
